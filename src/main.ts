c8a219ba49cd1152dec9f0cb80443360b60b97ea
Luigi Bastidas <bastidasluigi05@gmail.com>
2024-10-19 00:55:54 -0400

    Imagen de postgres

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.use(json({ limit: '200mb' }));
   await app.listen(3000);
 }
 bootstrap();
